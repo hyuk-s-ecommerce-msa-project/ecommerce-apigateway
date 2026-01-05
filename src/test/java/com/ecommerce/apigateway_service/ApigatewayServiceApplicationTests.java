5a0d4975f9f548bace705e442ca1b326f769165a
Hyuk <testerhyuk214@gmail.com>
2026-01-05 12:46:45 +0900

    initial api gateway

---
+++ src/test/java/com/ecommerce/apigateway_service/ApigatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.apigateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
